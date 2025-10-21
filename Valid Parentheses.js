5b67ff410d7d644705ac5b076ea0e3ff533b6883
Bryce Moran <128500879+BwithTea@users.noreply.github.com>
2025-10-21 11:17:30 -0400

    Refactor validParentheses function for clarity

--- Valid Parentheses.js
+++ Valid Parentheses.js
@@ -2,31 +2,39 @@
 
 function validParentheses(parenStr) {
   // Initialize a balance variable to keep track ok both Parenthesis
-  var balance = 0
+  let balance = 0
   
   // Split the str and create a new array
-  var parenArr = parenStr.split("")
+  let parenArr = parenStr.split("")
   
   // Iterate over the new Parenthesis array
-  for(i = 0; i < parenArr.length; i++) {
+  for (i = 0; i < parenArr.length; i++) {
     // Initialize each index to str variable
-    var str = parenArr[i]
+    let str = parenArr[i]
     console.log(str,i)
     
-    if(str === "(") { // Check if left Parenthesis
+    if (str === "(") { // Check if left Parenthesis
       console.log("Found left")
       balance++
       
-    }else if(str === ")"){ // Check if right Parenthesis
+    } else if (str === ")") { // Check if right Parenthesis
       console.log("Found right")
       balance--
     }
+    
     // Check if balance is negative
-    if(balance < 0){
+    if (balance < 0) {
       return false
     }
     console.log(balance)
   }
+  
   // Return true or false based on balance value
   return balance === 0
 }
+
+/* ========================
+   Potential optimizations 
+   ========================
+    Didn't need the string variable, so I could have just checked directly. 
+*/
