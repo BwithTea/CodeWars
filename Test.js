87389ab2aad44460401555e066f73f93e71dbfb8
Bryce Moran <128500879+BwithTea@users.noreply.github.com>
2025-10-31 23:09:52 -0400

    Fix variable name and loop in addNums function
    Fix variable name in addNums function and correct loop.

--- Test.js
+++ Test.js
@@ -1,11 +1,15 @@
+
+let numsArr = [1,3,10,20,5];
+
 function addNums () {
   let total = 0;
   for (i = 0; i < arr.length; i++) {
     total += arr[i];
-  
+    
     console.log(total);
   
   }
   return total
 }
 
+addNums(numsArr)
